f381832f7e5fddc705d47e4bf2a6c778fd64859d
kommeleon <177319311+kommeleon@users.noreply.github.com>
2026-02-19 16:02:35 +0100

    Bewegung und Kamera Rotation
    Nach ungefähr 2.5 Stunden habe ich Kamera Rotation und Bewegung hinzugefügt. Es funktioniert in dem das Programm nach jeder Bewegung den Abstand zwischen der Kamera und den Punkten berechnet und dann mithilfe eines Basisvektors, dass mit der Kamera rotiert, die Punkte rotiert. Sie werden dann auf den Bildschirm projeziert als "Wireframe" vom Würfel indem man sagt, welche Punkte verbunden sind. Es wurden auch verschieden Massnahmen hinzugefügt, dass die Kamera nicht überrotieren kann und dass es keine Punkte hinter der Kamera geben kann. Letzeres ist noch nicht richtig implementiert und sollte verbessert werden, Punkte hinter der Kamera verschwinden, gemeinsam mit den Linien die mit ihn verbunden sind.

--- screen_renderer.gd
+++ screen_renderer.gd
@@ -1,38 +1,70 @@
 extends Node2D
-var cube = [Vector3(0,0,1),Vector3(0,0,2),Vector3(1,0,2),Vector3(1,0,1),Vector3(0,1,1),Vector3(0,1,2),Vector3(1,1,2),Vector3(1, 1, 1)]
+var cube = [Vector3(0,0,1), Vector3(0,0,2), Vector3(1,0,2), Vector3(1,0,1), Vector3(0,1,1), Vector3(0,1,2), Vector3(1,1,2), Vector3(1, 1, 1)]
+var edges = [[0,1], [1,2], [2,3], [3,0],[4,5], [5,6], [6,7], [7,4],[0,4], [1,5], [2,6], [3,7]]
 var camera_pos = Vector3(0,0,0)
+var camera_rot = Vector2(0,0)
+var speed = 1
+var sensitivity = 5
+
 func _3d_to_2d(points_3d: Array) -> Array:
 	var flat_points = []
 	for point in points_3d:
-		var z_safe = max(point.z, 0.01)
-		var x_2d = point.x / z_safe
-		var y_2d = point.y / z_safe
-		flat_points.append(Vector2(x_2d, y_2d))
+		if point.z < 0.1:
+			flat_points.append(Vector2(-10000, -10000))
+		else:
+			var x_2d = point.x / point.z
+			var y_2d = point.y / point.z
+			flat_points.append(Vector2(x_2d, y_2d))
 	return flat_points
 
 func _points_from_cam(points_3d: Array, camera_pos: Vector3) -> Array:
 	var distance_points = []
+	var basis = Basis().rotated(Vector3(0, 1, 0), camera_rot.y)
+	basis = basis.rotated(basis.x, camera_rot.x)
+	var cam_inv = basis.inverse()
 	for point in points_3d:
 		var relative_vector = point - camera_pos
-		distance_points.append(relative_vector)
+		distance_points.append(cam_inv * relative_vector)
 	return distance_points
 	
 func _rendered_points(array,camera_pos):
 	return _3d_to_2d(_points_from_cam(array,camera_pos))
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		camera_rot.y -= event.relative.x * sensitivity/-1000
+		camera_rot.x -= event.relative.y * sensitivity/1000
+		camera_rot.x = clamp(camera_rot.x, -1.57, 1.57)
+
 
 func _process(delta):
-	camera_pos.z += delta * 0.1
-	camera_pos.x += delta * 0.1
-	camera_pos.y += delta * 0.1
+	var basis = Basis()
+	basis = basis.rotated(Vector3(0, 1, 0), camera_rot.y) # Yaw
+	basis = basis.rotated(basis.x, camera_rot.x) # Pitch
+	var forward = -basis.z 
+	var side = basis.x
+	if Input.is_action_pressed("ui_up"):
+		camera_pos -= forward * speed * delta
+	if Input.is_action_pressed("ui_down"):
+		camera_pos += forward * speed * delta
+	if Input.is_action_pressed("ui_left"):
+		camera_pos -= side * speed * delta
+	if Input.is_action_pressed("ui_right"):
+		camera_pos += side * speed * delta
+	
 	queue_redraw()
 
 func _draw():
 	var points_to_draw = _rendered_points(cube, camera_pos)
-	
 	var screen_center = get_viewport_rect().size / 2
 	var scale_amount = 200.0
 	
+	var screen_points = []
 	for p in points_to_draw:
-		var final_pos = (p * scale_amount) + screen_center
-		draw_circle(final_pos, 5.0, Color.WHITE)
+		screen_points.append((p * scale_amount) + screen_center)
+
+	for edge in edges:
+		var p1 = screen_points[edge[0]]
+		var p2 = screen_points[edge[1]]
+		if p1.x > -5000 and p2.x > -5000:
+			draw_line(p1, p2, Color.WHITE, 2.0)
