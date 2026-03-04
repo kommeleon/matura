a957af4e8db9afd5bc56d155b42559930045f062
kommeleon <177319311+kommeleon@users.noreply.github.com>
2026-03-05 00:16:03 +0100

    Kommentare
    ~10 min
    Kommentare hinzugefügt um code lesbarer zu machen

--- screen_renderer.gd
+++ screen_renderer.gd
@@ -6,41 +6,44 @@ var camera_rot = Vector2(0,0)
 var speed = 1
 var sensitivity = 5
 
+func _camera_rot_2d():
+	var basis = Basis().rotated(Vector3(0, 1, 0), camera_rot.y) # Rotation der Kamera nach links oder rechts
+	basis = basis.rotated(basis.x, camera_rot.x) # Rotation der Kamera nach oben oder unten
+	return basis
+
 func _3d_to_2d(points_3d: Array) -> Array:
-	var flat_points = []
+	var converted_points = []
 	for point in points_3d:
-		if point.z < 0.1:
-			flat_points.append(Vector2(-10000, -10000))
+		if point.z < 0.1: # Falls Punkt hinter Kamera, irgendswo hin teleportieren
+			converted_points.append(Vector2(-10000, -10000))
 		else:
 			var x_2d = point.x / point.z
 			var y_2d = point.y / point.z
-			flat_points.append(Vector2(x_2d, y_2d))
-	return flat_points
+			converted_points.append(Vector2(x_2d, y_2d))
+	return converted_points
 
 func _points_from_cam(points_3d: Array, camera_pos: Vector3) -> Array:
 	var distance_points = []
-	var basis = Basis().rotated(Vector3(0, 1, 0), camera_rot.y)
-	basis = basis.rotated(basis.x, camera_rot.x)
-	var cam_inv = basis.inverse()
-	for point in points_3d:
+	var basis = _camera_rot_2d()
+	var cam_inv = basis.inverse() # Sicht invertieren (nach links schauen, sachen bewegen sich nach rechts
+	for point in points_3d: # Abstand messen von Punkten zur Kamera:
 		var relative_vector = point - camera_pos
 		distance_points.append(cam_inv * relative_vector)
 	return distance_points
 	
 func _rendered_points(array,camera_pos):
+	# alle Funktionen auf die Punkte anwenden
 	return _3d_to_2d(_points_from_cam(array,camera_pos))
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		camera_rot.y -= event.relative.x * sensitivity/-1000
 		camera_rot.x -= event.relative.y * sensitivity/1000
-		camera_rot.x = clamp(camera_rot.x, -1.57, 1.57)
+		camera_rot.x = clamp(camera_rot.x, -1.57, 1.57) # keine Überrotation
 
 
 func _process(delta):
-	var basis = Basis()
-	basis = basis.rotated(Vector3(0, 1, 0), camera_rot.y) # Yaw
-	basis = basis.rotated(basis.x, camera_rot.x) # Pitch
+	var basis = _camera_rot_2d() # Rotation der Kamera für Bewegung
 	var forward = -basis.z 
 	var side = basis.x
 	if Input.is_action_pressed("ui_up"):
@@ -60,11 +63,11 @@ func _draw():
 	var scale_amount = 200.0
 	
 	var screen_points = []
-	for p in points_to_draw:
-		screen_points.append((p * scale_amount) + screen_center)
+	for i in points_to_draw:
+		screen_points.append((i * scale_amount) + screen_center)
 
-	for edge in edges:
-		var p1 = screen_points[edge[0]]
-		var p2 = screen_points[edge[1]]
+	for i in edges:
+		var p1 = screen_points[i[0]]
+		var p2 = screen_points[i[1]]
 		if p1.x > -5000 and p2.x > -5000:
 			draw_line(p1, p2, Color.WHITE, 2.0)
